02eeb946aaa67847ce4a6af369582744b928a877
user <user@user.user>
2026-02-20 14:31:55 +0300

    Progress

--- routes/login.go
+++ routes/login.go
@@ -2,9 +2,21 @@ package routes
 
 import (
 	"net/http"
-	"todoer/templates"
+	// "todoer/templates"
+	"html/template"
 )
 
 func Login(writer http.ResponseWriter, req *http.Request) {
-	templates.Execute(writer, "login", nil)
+	// templates.Execute(writer, "login", nil)
+	base, err := template.ParseFiles("templates/base_empty.html")
+	if err != nil {
+		panic(err)
+	}
+	data := struct { Username string } { Username: "admin" }
+	// data := struct { Bar string } { Bar: "bar" }
+	// base.ExecuteTemplate(writer, "base_empty", nil)
+	err = base.Execute(writer, data)
+	if err != nil {
+		panic(err)
+	}
 }

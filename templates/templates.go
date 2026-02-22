3e1e1b0ccc34295014b3f5112fb698ce8d358148
OrbitalOyster <orbitaloyster@gmail.com>
2026-02-22 10:01:21 +0300

    Working html templates+layouts+partials

--- templates/templates.go
+++ templates/templates.go
@@ -13,34 +13,40 @@ type TemplateDeclaration struct {
 }
 
 var layoutsGlob = filepath.Join("templates", "layouts", "*.html")
+var partialsGlob = filepath.Join("templates", "partials", "*.html")
 var pagesFolder = filepath.Join("templates", "pages")
+
 var layouts *template.Template
 var parsed = make(map[string]	*template.Template)
 
 func init()  {
-	/* Layouts */
-	log.Printf("Parsing layouts (%s)\n", layoutsGlob)
-	var err error
-	layouts, err = template.ParseGlob(layoutsGlob)
+	/* Parse layouts */
+	parsedLayouts, err := template.ParseGlob(layoutsGlob)
 	if err != nil {
 		panic(err)
 	}
+	/* Mix in partials */
+	layoutsWithPartials, err := parsedLayouts.ParseGlob(partialsGlob)
+	if err != nil {
+		panic(err)
+	}
+	layouts = layoutsWithPartials
 }
 
 func Add(name string, layout string, page string)  {
-	/* Get layout */
+	/* Clone layout */
 	clonedLayouts, err := layouts.Clone()
 	if err != nil {
 		panic(err)
 	}
-	/* Get page */
+	/* Parse page */
 	filename := filepath.Join(pagesFolder, page)
 	newPage, err := clonedLayouts.ParseFiles(filename)
 	if err != nil {
 		panic(err)
 	}
 	parsed[name] = newPage
-	log.Printf("Added page %s\n", name)
+	log.Printf("Added page \"%s\"\n", name)
 }
 
 func Execute(writer http.ResponseWriter, name string, data any) {

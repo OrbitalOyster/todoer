ce9a16d47dff19de88f866435bba6fa3c4c65a5c
user <user@user.user>
2026-02-25 10:33:09 +0300

    Datetime stuff

--- routes/main.go
+++ routes/main.go
@@ -2,18 +2,22 @@ package routes
 
 import (
 	"net/http"
-	"todoer/templates"
 	"todoer/jwt"
+	"todoer/tasks"
+	"todoer/templates"
 )
 
 func Main(writer http.ResponseWriter, req *http.Request) {
 	claims := jwt.Get(req)
+	taskList := tasks.Get()
 	data := struct {
 		Title string
 		Username string
+		Tasks []tasks.Task
 	} {
 		Title: "todoer",
 		Username: claims.UserID,
+		Tasks: taskList,
 	}
 	templates.Execute(writer, "main", data)
 }
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -4,20 +4,24 @@ import (
 	"github.com/goccy/go-yaml"
 	"log"
 	"os"
+	"time"
 )
 
 const tasksFilename = "tasks.yaml"
 
 type Task struct {
-	Id          int    `yaml:"id"`
-	User				string `yaml:"user"`
-	Description string `yaml:"description"`
-	Done        bool   `yaml:"done"`
+	Id                int    `yaml:"id"`
+	User              string `yaml:"user"`
+	Datetime          string `yaml:"datetime"`
+	DatetimeParsed    time.Time
+	DatetimeFormatted string
+	Description       string `yaml:"description"`
+	Done              bool   `yaml:"done"`
 }
 
 var list []Task
 
-func Load()  {
+func Load() {
 	log.Println("Loading tasks from", tasksFilename)
 	/* Load raw yaml */
 	listRaw, err := os.ReadFile(tasksFilename)
@@ -28,7 +32,20 @@ func Load()  {
 	if err := yaml.Unmarshal(listRaw, &list); err != nil {
 		panic(err)
 	}
+	/* Format datetime */
+	for i, t := range list {
+		t, err := time.Parse(time.DateTime, t.Datetime)
+		if err != nil {
+			panic(err)
+		}
+		list[i].DatetimeParsed = t
+		list[i].DatetimeFormatted = t.Format("2.01.2006 15:04:05")
+	}
 	log.Println("Tasks found:", len(list))
+}
+
+func Get() []Task {
+	return list
 }
 
 func Save() {

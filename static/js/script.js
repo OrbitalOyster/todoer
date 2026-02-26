4b91ee681ac712ec2055b0666a6c3edc793cb6e2
user <user@user.user>
2026-02-26 11:56:33 +0300

    Toasts

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,27 @@
 'use strict'
 
 let confirmMsg = null,
-  htmxConfirmMsg = null;
+  htmxConfirmMsg = null,
+  addToast = null;
 
 (function () {
 
   /* Set up bootstrap tooltips */
-  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-  [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  const tooltipTriggerList = 
+    document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  [...tooltipTriggerList].map(
+    tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)
+  )
+
+  /* Set up bootstrap toasts */
+  const toastElList = document.querySelectorAll('.toast');
+  [...toastElList].map(toastEl => new bootstrap.Toast(toastEl))
 
   /* Toggle password buttons */
-  const togglePasswordButtons = document.getElementsByClassName('toggle-password-btn')
-  for (let i = 0; i < togglePasswordButtons.length; i++)
-    togglePasswordButtons[i].addEventListener('click', togglePassword)
+  const togglePasswordBtns = 
+    document.getElementsByClassName('toggle-password-btn')
+  for (let i = 0; i < togglePasswordBtns.length; i++)
+    togglePasswordBtns[i].addEventListener('click', togglePassword)
 
   function togglePassword() {
     const input = this.previousElementSibling
@@ -27,13 +36,13 @@ let confirmMsg = null,
   /* Confirm modal */
   confirmMsg = async (title, content) => {
     const modal = new bootstrap.Modal('#confirmModal'),
-      modalDiv = document.getElementById('confirmModal'),
-      titleH = modalDiv.querySelector('#modalTitle'),
-      contentDiv = modalDiv.querySelector('#modalContent'),
-      okButton = modalDiv.querySelector('#modalOkBtn'),
-      cancelButton = modalDiv.querySelector('#modalCancelBtn')
-    titleH.textContent = title
-    contentDiv.textContent = content
+      modalEl = document.getElementById('confirmModal'),
+      titleEl = modalDiv.querySelector('#modalTitle'),
+      contentEl = modalDiv.querySelector('#modalContent'),
+      okBtn = modalEl.querySelector('#modalOkBtn'),
+      cancelBtn = modalDiv.querySelector('#modalCancelBtn')
+    titleEl.textContent = title
+    contentEl.textContent = content
     modal.show()
     return new Promise((resolve) => {
       const ok = () => {
@@ -42,17 +51,46 @@ let confirmMsg = null,
         },
         cancel = () => cleanUp(),
         cleanUp = () => {
-          okButton.removeEventListener('click', ok)
-          cancelButton.removeEventListener('click', cleanUp)
+          okBtn.removeEventListener('click', ok)
+          cancelBtn.removeEventListener('click', cleanUp)
           modal.hide()
         }
-      okButton.addEventListener('click', ok)
-      cancelButton.addEventListener('click', cancel)
+      okBtn.addEventListener('click', ok)
+      cancelBtn.addEventListener('click', cancel)
     })
   }
 
-  // htmxConfirmMsg = (title, content) => {
-  //   confirmMsg(title, content).then(res => res && htmx.trigger(this, 'confirmed'))
-  // }
+  /* Confirm modal for htmx events */
+  htmxConfirmMsg = (el, title, content) => 
+    confirmMsg(title, content)
+      .then(res => res && htmx.trigger(el, 'confirmed'))
+
+  addToast = (title, msg) => {
+    const toastEl = document.createElement('div'),
+      toastHeader = document.createElement('div'),
+      toastTitle = document.createElement('strong'),
+      toastCloseBtn = document.createElement('button'),
+      toastBody = document.createElement('div')
+
+    toastEl.className = 'toast'
+    toastEl.dataset.bsAutohide = 'false'
+    toastHeader.className = 'toast-header'
+    toastTitle.textContent = title
+    toastTitle.className = 'me-auto'
+    toastCloseBtn.type = 'button'
+    toastCloseBtn.className = 'btn-close'
+    toastCloseBtn.dataset.bsDismiss = 'toast'
+    toastBody.className = 'toast-body'
+    toastBody.textContent = msg
+
+    toastEl.appendChild(toastHeader)
+    toastHeader.appendChild(toastTitle)
+    toastHeader.appendChild(toastCloseBtn)
+    toastEl.appendChild(toastBody)
+
+    document.querySelector('.toast-container').appendChild(toastEl)
+    new bootstrap.Toast(toastEl).show()
+    toastEl.addEventListener('hidden.bs.toast', () => toastEl.remove())
+  }
 
 })()

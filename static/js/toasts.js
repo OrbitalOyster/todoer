56a29ba08076a404713acadc49645cf5f74d59af
user <user@user.user>
2026-09-14 16:16:50 +0300

    Multiple toasts fix

--- static/js/toasts.js
+++ static/js/toasts.js
@@ -2,27 +2,30 @@
 
 /* On toast */
 const onToast = () => {
-	/* Get new toast */
-	const toastEl = document.querySelector(".toast-container").lastElementChild
-	/* Show it */
-	new bootstrap.Toast(toastEl).show()
-	/* Handle funky stuff */
-	const progressBar = toastEl.querySelector(".progress-bar")
-	if (progressBar) {
-		progressBar.style = "width: 100%; transition: width linear 10s"
-		/* Halt progress bar animation on mouse over */
-		toastEl.addEventListener(
-			"mouseover",
-			() => (progressBar.style = "width: 0%; transition: none"),
-		)
-		/* Resume on mouse leave */
-		toastEl.addEventListener(
-			"mouseleave",
-			() => (progressBar.style = "width: 100%; transition: width linear 10s"),
-		)
-	}
-	/* Remove element after delay */
-	toastEl.addEventListener("hidden.bs.toast", toastEl.remove)
+	const toastContainer = document.getElementById("toast-container"),
+		toastEls = [...toastContainer.querySelectorAll(".toast:not(.fade)")]
+	/* For every new toast */
+	toastEls.forEach((toastEl) => {
+		/* Show it */
+		new bootstrap.Toast(toastEl).show()
+		/* Handle funky stuff */
+		const progressBar = toastEl.querySelector(".progress-bar")
+		if (progressBar) {
+			progressBar.style = "width: 100%; transition: width linear 10s"
+			/* Halt progress bar animation on mouse over */
+			toastEl.addEventListener(
+				"mouseover",
+				() => (progressBar.style = "width: 0%; transition: none"),
+			)
+			/* Resume on mouse leave */
+			toastEl.addEventListener(
+				"mouseleave",
+				() => (progressBar.style = "width: 100%; transition: width linear 10s"),
+			)
+		}
+		/* Remove element after delay */
+		toastEl.addEventListener("hidden.bs.toast", toastEl.remove)
+	})
 }
 
 const initToasts = () => {
